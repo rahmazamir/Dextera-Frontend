2d03148ada939258fb277923d79ee6235abd6cc6
Rahma <rzamir512@gmail.com>
2026-05-03 20:52:09 +0500

    remove base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Dextera-Frontend/',
   plugins: [react()],
   server: {
     headers: {
